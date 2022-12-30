e2ae8d6e162e0f5c3f7b1b14e985a4ed0a17b008
omidgholami74 <omid.gholami74@hotmail.com>
2022-12-30 14:31:20 +0330

    DIP

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Digital-image-processing" 
+"# Digital-image-processing" 
--- sahar.m
+++
@@ -1,4 +0,0 @@
-sample = rand(3,3);
-rowNames = {'a','b','c'};
-colNames = {'x','y','z'};
-sTable = array2table(sample,'RowNames',rowNames,'VariableNames',colNames)
